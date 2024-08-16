e23840bfc843e6028e1a340ef0a85f17d039b2d8
Edward14 <edward.chang.85y@gmail.com>
2024-08-16 17:15:07 +0800

    test

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import os
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
-    """Example Hello World route."""
+    """Example Hello World route with an image."""
     name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
-
+    return render_template("index.html", name=name)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
