a8e5071adcbc2ec5d5aec68346ee11f28d291d47
Edward14 <edward.chang.85y@gmail.com>
2024-08-16 17:02:13 +0800

    test2

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask
 import os
+
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
-    # 从环境变量中获取 PORT 值，默认是 8080
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
